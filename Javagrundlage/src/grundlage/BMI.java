cdd1e67781bfab1d2c4df5f085410d8a28cad7ff
Martin Wischhusen <m.p.wischhusen@gmx.de>
2024-07-01 08:46:44 +0200

    Add Scanner Methode. Ich habe auch versucht die IO Methode zu nutzen, scheiterte aber. Habe die Versuche in Kommentare hinterlegt.

--- Javagrundlage/src/grundlage/BMI.java
+++ Javagrundlage/src/grundlage/BMI.java
@@ -1,32 +1,67 @@
 package grundlage;
 
 import java.util.Scanner;
+import java.io.*;
 
 public class BMI {
 
 	
 	public static void main (String[] args) {
-		
-	Scanner scanner = new Scanner (System.in);
+	//public static void IO (double height, double weight, double bmi) {
 	
-
-	 System.out.println ("What's your Name? ");
-	 String name = scanner.next();
+	Scanner scanner = new Scanner(System.in);
+	
+	System.out.println ("What's your Name? ");
+	String name = scanner.next();
+	
+	//IO.promptAndReadString("What's your name? + + );
+	
+	
+	System.out.println("How tall are you?"
+			+ " Please tell me in meter.");
+	Double height = scanner.nextDouble();
+	// size in m
+	
+	
+	
+	
+	System.out.println("What your weight?"
+			+ "Please tell me in kg.");
+	Double weight = scanner.nextDouble();
+	 //  weight in KG	
+			
+	
+	Double bmi = weight / (height * height);
+	System.out.println("Hello" + name);
+	System.out.println("Your BMI is: "+bmi);
+	System.out.println("Without the numbers after the comma is your BMI: " + Math.round(bmi));
 	 
- 	System.out.println("Eingabe Gewicht in weight: +m+ ");
- 	double weight = 100.5 ;
+//	IO.promptAndReadDouble("Bitte ... eingeben: ");
+// 	System.out.println("What's your weight in KG?" + weight + );
+// 	 double weight = scanner.nextDouble() ;
  	
-	System.out.println("Eingabe Größe in cm: +size+ ");
-	double size = 1.90 ;
+ 	
 
-	 //  weight in KG
-	 // size in m
-		
-	double bmi = weight / (size  * size);
 
-	System.out.println("Your BMI is: "+bmi+" punkte");
-	// scanner.close();
 	
-	}
 		
-}   
+//	double bmi = weight / (size  * size);
+//	IO.writeln("Wert = "+ bmi);
+//	System.out.println("Bonjour" + name);
+//	System.out.println("Your BMI is: "+bmi+" punkte");
+//	System.out.println("Without those numbers after the comma, the BMI Count is" + Math.round(bmi));
+	
+	
+	
+	
+	
+	
+	/*public static void inFileStream(File name, File weight, File size) {
+		InputStream namestream= null;
+		InputStream weightstream= null;
+		Inputstream sizestream= null;
+	*/	
+		scanner.close();
+	}	
+	}
+   
